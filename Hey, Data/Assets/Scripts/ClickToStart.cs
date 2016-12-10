a69ccbccf7ef7896a80232a2806390183cae7538
Phillammon <alistair@hippotigris.org.uk>
2016-12-10 13:45:36 +0000

    This is actually pretty relaxing

---
+++ Hey, Data/Assets/Scripts/ClickToStart.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickToStart : MonoBehaviour {
+	public GameObject menuPane;
+	private bool started;
+	// Use this for initialization
+	void Start () {
+		started = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void StartGame(){
+		if (!started) {
+			started = true;
+			StartCoroutine (FadeText ());
+		}
+	}
+
+	IEnumerator FadeText()
+	{
+		CanvasGroup cgroup = menuPane.GetComponent<CanvasGroup> ();
+		float aTime = 2.0f;
+		float alpha = cgroup.alpha;
+		for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / aTime)
+		{
+			cgroup.alpha = Mathf.Lerp(alpha,0f,t);
+			yield return null;
+		}
+	}
+}
---
+++ Hey, Data/Assets/Scripts/MuteToggle.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MuteToggle : MonoBehaviour {
+
+	// Use this for initialization
+	public Sprite mutesprite;
+	public Sprite unmutesprite;
+	public Button button;
+	private bool muted;
+	void Start () {
+		muted = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ToggleSound(){
+		muted = !muted;
+		AudioListener.pause = muted;
+		if (muted) {
+			button.image.sprite = mutesprite; 
+		}
+		else {
+			button.image.sprite = unmutesprite; 
+		}
+	}
+}
