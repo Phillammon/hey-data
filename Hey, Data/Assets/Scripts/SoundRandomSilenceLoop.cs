8b50e6c10e4346282cf1307cc1b50c215ea4abc2
Phillammon <alistair@hippotigris.org.uk>
2016-12-10 12:03:47 +0000

    Bunch of assets, basic ambience

---
+++ Hey, Data/Assets/Scripts/SoundRandomSilenceLoop.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundRandomSilenceLoop : MonoBehaviour {
+	public AudioClip clip;
+	public AudioSource source;
+	public float minTime;
+	public float maxTime;
+
+	// Use this for initialization
+	void Start () {
+		source.clip = clip;
+		StartCoroutine (LoopWithSilence());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator LoopWithSilence(){
+		yield return new WaitForSeconds (clip.length + UnityEngine.Random.Range (minTime, maxTime));
+		source.Play ();
+		StartCoroutine (LoopWithSilence());
+	}
+}
